7791240e88fd83fe1289a7d2e11a190014479419
LucianoEsplai <lfrancoepifani@gmail.com>
2024-08-06 13:56:37 +0200

    deploying email manager

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  },
+  adapter: adapter({
+    out: 'build' // La carpeta de salida de la construcción
+  }),
   preprocess: vitePreprocess()
 };
 export default config;
